03e6a7b194d1cdb8d524c320426202db069cb29f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-13 13:13:24 -0500

    configuracion inicial

---
+++ src/main/java/org/api/api_autores_libros/ApiAutoresLibrosApplication.java
@@ -0,0 +1,13 @@
+package org.api.api_autores_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAutoresLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAutoresLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/api/api_autores_libros/ApiAutoresLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.api_autores_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutoresLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
