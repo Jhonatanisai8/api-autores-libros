d9f89db65097f62e73fb1f471f3508ce8c22edd2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-13 14:08:41 -0500

    service de autor: implementacio del metodo para obtener un autor por su id

--- src/main/java/org/api/api_autores_libros/services/AuthorService.java
+++ src/main/java/org/api/api_autores_libros/services/AuthorService.java
@@ -7,6 +7,8 @@ import org.api.api_autores_libros.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class AuthorService {
 
@@ -15,5 +17,15 @@ public class AuthorService {
 
     public List<Author> getAllAuthors() {
         return repository.findAll();
+    }
+
+    public Author geAuthorById(Long idAuthor) {
+        return getAllAuthors()
+                .stream()
+                .filter(author -> author.getId().equals(idAuthor))
+                .findFirst()
+                .map(authorBus -> {
+                    return authorBus;
+                }).orElseThrow(() -> new EntityNotFoundException("Autor con no encontrado con ID: " + idAuthor));
     }
 }
--- src/main/java/org/api/api_autores_libros/services/BookService.java
+++ src/main/java/org/api/api_autores_libros/services/BookService.java
@@ -1,11 +1,16 @@
 package org.api.api_autores_libros.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.api.api_autores_libros.model.Author;
 import org.api.api_autores_libros.model.Book;
+import org.api.api_autores_libros.repository.AuthorRepository;
 import org.api.api_autores_libros.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class BookService {
@@ -13,8 +18,22 @@ public class BookService {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     public List<Book> getAllBooks() {
         return repository.findAll();
+    }
+
+    public Book saveBook(Book request) {
+        Optional<Author> authorSearch = authorRepository
+                .findById(request.getAuthor().getId());
+        if (!authorSearch.isPresent()) {
+            throw new EntityNotFoundException("Autor no encontrado con Id: " + request.getAuthor()
+                    .getId());
+        }
+        request.setAuthor(authorSearch.get());
+        return repository.save(request);
     }
 
 }
