38b04b5f9dedc2ad141a94e006f2e133dd10e5e2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-13 13:43:11 -0500

    repositorios para las entidades de autor y libro

---
+++ src/main/java/org/api/api_autores_libros/repository/AuthorRepository.java
@@ -0,0 +1,11 @@
+package org.api.api_autores_libros.repository;
+
+import org.api.api_autores_libros.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository
+        extends JpaRepository<Author, Long>{ 
+
+}
